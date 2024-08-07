4530f586a0be6c8904d6af9ba12d5ac11143e5a5
Zildjian Vito <zildjianvito07@gmail.com>
2024-08-07 13:22:24 +0000

    fix: server liux

--- src/index.ts
+++ src/index.ts
@@ -11,8 +11,7 @@ const client = new Client({
       "https://raw.githubusercontent.com/wppconnect-team/wa-version/main/html/2.2412.54.html",
   },
   puppeteer: {
-    executablePath:
-      "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
+    executablePath: "/usr/bin/google-chrome-stable"
     // "C:\\Program Files\\Google\\Chrome\\Application\\chrome.exe",
     headless: true,
     args: ["--no-sandbox", "--disable-setuid-sandbox"],
@@ -28,7 +27,7 @@ client.on("ready", () => {
 });
 
 client.on("group_join", async (notification) => {
-  // if (notification.chatId === "120363320654299029@g.us") {
+  if (notification.chatId === "120363320654299029@g.us") {
   const chatId = notification.chatId;
 
   const welcomeMessage = `Selamat datang di grup Volunteer SRIFOTON 2024!!,
@@ -36,7 +35,7 @@ semoga betah!!`;
 
   client.sendMessage(chatId, welcomeMessage);
 
-  // }
+   }
 });
 
 client.on("message_create", async (msg) => {
